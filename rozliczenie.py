f8d3b52ca86ac03a9c92d2bae9c201b3976e9fe3
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 15:01:31 +0100

    zadanie

---
+++ rozliczenie.py
@@ -0,0 +1,16 @@
+import os
+
+with open(f'{os.getcwd()}\\rozliczenie-1.csv') as plik:
+    plik.readline()
+    data = plik.readlines()
+
+sum = 0
+na_macierzynskim = 0
+for index in range(0, len(data)):
+    items = data[index].split(',')
+    sum += int(items[1])
+    if str.strip(items[4]) == 'n':
+        na_macierzynskim += 1
+
+print(f'Srednia: {round(sum / len(data), 2)}')
+print(f'Na macierzynskim:{na_macierzynskim}')
