309ac62a905f12655cc3331c13fc53fa72aa3d4b
krzysztof <krzysztof.szymczyk@yahoo.com>
2022-12-10 16:51:45 +0100

    Another solution

---
+++ rozliczenie_inne_podejscie.py
@@ -0,0 +1,15 @@
+import os
+
+with open(os.path.join(os.getcwd(), 'rozliczenie-1.csv'), encoding="utf16", errors='ignore') as plik:
+    data = plik.readlines()
+
+sum = 0
+na_macierzynskim = 0
+for index in range(1, len(data)):
+    items = data[index].split(',')
+    sum += int(items[1])
+    if str.strip(items[4]) == 'n':
+        na_macierzynskim += 1
+
+print(f'Srednia: {round(sum / (len(data) - 1), 2)}')
+print(f'Na macierzynskim:{na_macierzynskim}')
