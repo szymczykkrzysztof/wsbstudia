ccf82bfe611cf76cf1388d5f3bdf579316d6b6c3
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-11 13:30:35 +0100

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz(data):
+    number = int(data)
+
+    if number % 5 == 0 and number % 3 == 0:
+        return "FizzBuzz"
+    elif number % 5 == 0:
+        return "Buzz"
+    elif number % 3 == 0:
+        return "Fizz"
+    else:
+        return number
---
+++ fizzbuzz_test.py
@@ -0,0 +1,21 @@
+from fizzbuzz import fizzbuzz
+
+
+def test_0():
+    assert fizzbuzz(1) == 1
+
+
+def test_1():
+    assert fizzbuzz(3) == "Fizz"
+
+
+def test_2():
+    assert fizzbuzz(5) == "Buzz"
+
+
+def test_3():
+    assert fizzbuzz(15) == "FizzBuzz"
+
+
+def test_4():
+    assert fizzbuzz(10) == "Buzz"
